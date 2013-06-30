fb18548757eb8f19f13631462e8cfcb833679ee9
paulo carmona <pcarmonaz@hotmail.com>
2013-06-29 19:04:33 -0500

    Inicio App SendMail- envio de correo Rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendmail::Application.config.secret_token = 'dcee799fd5bee1a2049beabfd781eb9a33f4bd0300d3ed335fc49c1d3bf93bcc60e70d2153e1e057c8548fd8652425bc500fc539636bae1d76cd470a494a5ec4'
